037d7272d3f1aa174d4f4dc2540c37393329884a
Gwyn Jones <m@gwnjns.com>
2018-06-12 09:30:01 +0100

    Add security group

--- resources.tf
+++ resources.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "environment-example" {
   }
 }
 
-resource "aws_subnet" "subnet1" { 
+resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block, 3, 1)}"
   vpc_id = "${aws_vpc.environment-example.id}"
   availability_zone = "eu-west-2a"
@@ -17,4 +17,19 @@ resource "aws_subnet" "subnet2" {
   cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block, 2, 2)}"
   vpc_id = "${aws_vpc.environment-example.id}"
   availability_zone = "eu-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example.id}"
+
+  ingress {
+
+    cidr_blocks = [
+      "${aws_vpc.environment-example.cidr_block}"
+    ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
 }
