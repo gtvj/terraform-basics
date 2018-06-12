3b931f14485910f93aa49d5dff995123bfd6c0f5
Gwyn Jones <m@gwnjns.com>
2018-06-12 09:49:42 +0100

    Extract subnets to dedicated configuration file

--- resources.tf
+++ resources.tf
@@ -7,18 +7,6 @@ resource "aws_vpc" "environment-example" {
   }
 }
 
-resource "aws_subnet" "subnet1" {
-  cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block, 3, 1)}"
-  vpc_id = "${aws_vpc.environment-example.id}"
-  availability_zone = "eu-west-2a"
-}
-
-resource "aws_subnet" "subnet2" {
-  cidr_block = "${cidrsubnet(aws_vpc.environment-example.cidr_block, 2, 2)}"
-  vpc_id = "${aws_vpc.environment-example.id}"
-  availability_zone = "eu-west-2b"
-}
-
 resource "aws_security_group" "subnetsecurity" {
   vpc_id = "${aws_vpc.environment-example.id}"
 
